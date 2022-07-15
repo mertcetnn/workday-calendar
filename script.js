7242ffde8396b2c5baad1473ea8ca1dbb0f78dd0
MertCetin <mert.cetnn@gmail.com>
2022-07-15 00:35:23 -0400

    decs and live timer ready

---
+++ script.js
@@ -0,0 +1,19 @@
+var c9am=document.querySelector('nine');
+var c10am=document.querySelector('ten');
+var c11am=document.querySelector('eleven');
+var noon12=document.querySelector('twelve');
+var c1pm=document.querySelector('thirteen');
+var c2pm=document.querySelector('fourteen');
+var c3pm=document.querySelector('fifthteen');
+var c4pm=document.querySelector('sixteen');
+var c5pm=document.querySelector('seventeen');
+var btn=document.querySelector('.btnn')
+
+var currentDate=document.querySelector('currentDate')
+var textArea=document.querySelector('userEvent')
+
+ currentDate= new Date();
+ document.getElementById("currentDate").innerHTML = currentDate;
+
+
+
