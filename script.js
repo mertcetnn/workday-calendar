335bb449d7cebab8a4fe373b31e7d738d437cfe6
MertCetin <mert.cetnn@gmail.com>
2022-07-18 21:53:08 -0400

    textareas values added

--- script.js
+++ script.js
@@ -1,45 +1,61 @@
 // army style id declerations
-var c9am=document.querySelector('nine');
-var c10am=document.querySelector('ten');
-var c11am=document.querySelector('eleven');
-var noon12=document.querySelector('twelve');
-var c1pm=document.querySelector('thirteen');
-var c2pm=document.querySelector('fourteen');
-var c3pm=document.querySelector('fifthteen');
-var c4pm=document.querySelector('sixteen');
-var c5pm=document.querySelector('seventeen');
-var userEvent=c9am,c10am,c11am,noon12,c1pm,c2pm,c3pm,c4pm,c5pm
+var c9am = document.querySelector("nine");
+var c10am = document.querySelector("ten");
+var c11am = document.querySelector("eleven");
+var noon12 = document.querySelector("twelve");
+var c1pm = document.querySelector("thirteen");
+var c2pm = document.querySelector("fourteen");
+var c3pm = document.querySelector("fifthteen");
+var c4pm = document.querySelector("sixteen");
+var c5pm = document.querySelector("seventeen");
+var userEvent = c9am,
+  c10am,
+  c11am,
+  noon12,
+  c1pm,
+  c2pm,
+  c3pm,
+  c4pm,
+  c5pm;
 
-var currentDate=document.querySelector('#currentDate');
+var currentDate = document.querySelector("#currentDate");
 //current time function
-currentDate= new Date();
+currentDate = new Date();
 document.getElementById("currentDate").innerHTML = currentDate;
 
 
-document.getElementById('mybtn').addEventListener('click',function(onOrof){
-  
-;
- 
-function onOrof (){
-   if(userEvent.value>0){
-    onOrof ()
-      window.localStorage.setItem(userEvent,value)
-   let saving =localStorage.getItem(userEvent)
+//saving thru localstorage
+document.getElementById("mybtn").addEventListener("click", function () {
    
-   }
- if (userEvent==0){
-   localStorage.removeItem(userEvent)
-   localStorage.clear()
+        var c9am=document.getElementById('nine').value
+			localStorage.setItem("text9" ,c9am );
+        
 
- }}})
+         var c10am=document.getElementById('ten').value
+         localStorage.setItem('text10',c10am);
 
+         var c11am=document.getElementById('eleven').value
+         localStorage.setItem('text11',c11am);
 
+         var noon12=document.getElementById('twelve').value
+         localStorage.setItem('textnoon',noon12);
 
+         var c1pm=document.getElementById('thirthteen').value
+         localStorage.setItem('text13',c1pm);
 
+         var c2pm=document.getElementById('fourteen').value
+         localStorage.setItem('text14',c2pm);
 
+         var c3pc=document.getElementById('fifthteen').value
+         localStorage.setItem('text15',c3pm);
 
+         var c4pm=document.getElementById('sixteen').value
+         localStorage.setItem('text16',c4pm);
 
+         var c5pm=document.getElementById('seventeen').value
+         localStorage.setItem('text17',c5pm);
 
-
-
-
+ 
+    }
+  
+);
