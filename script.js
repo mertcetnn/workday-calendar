5053657c4974f8901a890fd73de9b94a5e1d90ee
MertCetin <mert.cetnn@gmail.com>
2022-07-18 20:28:28 -0400

    button added listener

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// army style id declerations
 var c9am=document.querySelector('nine');
 var c10am=document.querySelector('ten');
 var c11am=document.querySelector('eleven');
@@ -7,13 +8,26 @@ var c2pm=document.querySelector('fourteen');
 var c3pm=document.querySelector('fifthteen');
 var c4pm=document.querySelector('sixteen');
 var c5pm=document.querySelector('seventeen');
-var btn=document.querySelector('.btnn')
 
-var currentDate=document.querySelector('currentDate')
-var textArea=document.querySelector('userEvent')
+var currentDate=document.querySelector('#currentDate');
+//current time function
+currentDate= new Date();
+document.getElementById("currentDate").innerHTML = currentDate;
 
- currentDate= new Date();
- document.getElementById("currentDate").innerHTML = currentDate;
+function save(event) {
+   console.log(event)
+   let idx = event.target.id.replace(".user-event-", "")
+   let textInput = $(`#text-${idx}`).val();
+   localStorage.setItem(`Task${idx}`, textInput);
+}
+document.getElementById('mybtn').addEventListener('click',save())
+
+
+
+
+
+
+
 
 
 
