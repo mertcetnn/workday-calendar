686d754ab4980dd87273073b600176d7cd634320
MertCetin <mert.cetnn@gmail.com>
2022-07-18 21:14:11 -0400

    working on local storage

--- script.js
+++ script.js
@@ -8,19 +8,31 @@ var c2pm=document.querySelector('fourteen');
 var c3pm=document.querySelector('fifthteen');
 var c4pm=document.querySelector('sixteen');
 var c5pm=document.querySelector('seventeen');
+var userEvent=c9am,c10am,c11am,noon12,c1pm,c2pm,c3pm,c4pm,c5pm
 
 var currentDate=document.querySelector('#currentDate');
 //current time function
 currentDate= new Date();
 document.getElementById("currentDate").innerHTML = currentDate;
 
-function save(event) {
-   console.log(event)
-   let idx = event.target.id.replace(".user-event-", "")
-   let textInput = $(`#text-${idx}`).val();
-   localStorage.setItem(`Task${idx}`, textInput);
-}
-document.getElementById('mybtn').addEventListener('click',save())
+
+document.getElementById('mybtn').addEventListener('click',function(onOrof){
+  
+;
+ 
+function onOrof (){
+   if(userEvent.value>0){
+    onOrof ()
+      window.localStorage.setItem(userEvent,value)
+   let saving =localStorage.getItem(userEvent)
+   
+   }
+ if (userEvent==0){
+   localStorage.removeItem(userEvent)
+   localStorage.clear()
+
+ }}})
+
 
 
 
